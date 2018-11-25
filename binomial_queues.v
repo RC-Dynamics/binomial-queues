ffe5902b3cc96b8393ef456da82e308e6f6ab573
wmr1 <wmr@cin.ufpe.br>
2018-11-25 16:45:22 -0300

    Removed unused proofs, updated carry_valid

--- binomial_queues.v
+++ binomial_queues.v
@@ -2,10 +2,31 @@ Require Import Coq.Strings.String.
 Open Scope nat_scope.
 Open Scope list_scope.
 
+(************************************************************************)
+(* Copyright 2018 Nilson de Lima, Walber de Macedo, Orlando Neto        *)
+(* MIT License                                                          *)
+(************************************************************************)
+
+(* As funções e provas já estão em ordem de execução*)
+
+(* Uma lista de prioridade (priqueue), é um tipo de dados que visa simplificar as operações de junção de duas ávores ordenadas. Essa priq é uma lista de árvores que deve obedecer os requisitos:
+
+    1- A lista não deve ter seus elementos com tamanho igual. Cada posição da lista corresponde a uma altura diferente de uma árvore.
+
+    2- As árvores precisam ser uma power of 2 heap (pow2heap). Essa power of 2 heap é apenas uma árvore ordenada como max heap e sua raiz tem seu filho a direita uma árvore vazia, e seu filho a esquerda uma árvore completa. Isso faz com que as árvores tenham uma altura máxima de n. Olhando para a priqueue, temos que para cada índice da lista é o numero n da árvore: a posição 0 da arvore deve ter uma árvore de altura 0 e assim por diante.
+
+   Essa definição facilita a inserção e junção de duas priqueue, fazendo com que a junção seja reduzida a junção de árvores binárias, resultando em um algoritmo parecido com uma soma binária, onde caso uma posição da lista exceda o seu valor máximo de elementos, toda essa árvore seja passada para a próxima posição e tente ser encaixada nessa nova árvore.
+*)
+
+(* Representação simplificada de a maior que ou igual a b para ser usada *)
+
 Notation "a >? b" := (Nat.ltb b a)
                      (at level 70, only parsing) : nat_scope.
  
 Definition key := nat.
+
+(* Uma árvore é definida como para uma chave, existem duas subárvores onde uma folha não 
+   possui chave nem subárvores *)
 
 Inductive tree : Type :=
 | Node: key -> tree -> tree -> tree
@@ -15,6 +36,8 @@ Definition priqueue := list tree.
 
 Definition empty : priqueue := nil.
 
+(* Um smash representa a junção de duas árvores, fazendo sempre a ordenação ser preservada *)
+
 Definition smash (t u: tree) : tree :=
   match t , u with
   | Node x t1 Leaf, Node y u1 Leaf =>
@@ -22,6 +45,8 @@ Definition smash (t u: tree) : tree :=
                                 else Node y (Node x t1 u1) Leaf
   | _ , _ => Leaf
   end.
+
+(* Carry é a fase em que uma árvore não pode ficar mais em sua posição atual por ter excedido o tamanho máximo, então ela é passada para a próxima posição sendo somada à próxima árvore. Isso é feito recursivamente até que a árvore resultante tenha tamanho suficiente para ser acomodada naquela posição. *)
 
 Fixpoint carry (q: list tree) (t: tree) : list tree :=
   match q, t with
@@ -32,8 +57,14 @@ Fixpoint carry (q: list tree) (t: tree) : list tree :=
   | u :: q', _ => Leaf :: carry q' (smash t u)
  end.
 
+(* Um insert é apenas uma inserção em uma priqueue, o novo numero é colocado na primeira árvore e é visto
+   se a árvore resultante pode ser encaixada nessa posição, utilizando o carry. *)
+
 Definition insert (x: key) (q: priqueue) : priqueue :=
      carry q (Node x Leaf Leaf).
+
+
+(*  *)
 
 Fixpoint unzip (t: tree) (cont: priqueue -> priqueue) : priqueue :=
   match t with
@@ -47,6 +78,8 @@ Compute (unzip (Node 9
           (Node 5 (Node 3 (Node 2 Leaf Leaf) (Node 2 Leaf Leaf))
              (Node 3 (Node 1 Leaf Leaf) (Node 1 Leaf Leaf)))) (fun u => u)).
 
+(* Definição para a função de remoção do valor máximo de uma max heap.  *)
+
 Definition heap_delete_max (t: tree) : priqueue :=
   match t with
   | Node x t1 Leaf => unzip t1 (fun u => u)
@@ -56,6 +89,8 @@ Definition heap_delete_max (t: tree) : priqueue :=
 Compute (heap_delete_max (Node 9
           (Node 4 (Node 3 (Node 1 Leaf Leaf) (Node 1 Leaf Leaf))
              (Node 3 (Node 2 Leaf Leaf) (Node 5 Leaf Leaf))) Leaf)).
+
+(* Find max retorna o valor máximo em uma priqueue. *)
 
 Fixpoint find_max' (current: key) (q: priqueue) : key :=
   match q with
@@ -79,6 +114,8 @@ Compute (find_max (Node 1 Leaf Leaf
                      (Node 3 (Node 2 Leaf Leaf) (Node 5 Leaf Leaf))) Leaf
                 :: nil)).
 
+(*  *)
+
 Fixpoint join (p q: priqueue) (t: tree) : priqueue :=
   match p, q, t with
   | nil, _, _ => carry q t
@@ -91,6 +128,8 @@ Fixpoint join (p q: priqueue) (t: tree) : priqueue :=
   | p1 :: p', q1 :: q', _ => t :: join p' q' (smash p1 q1)
   end.
   
+(*  *)
+
  Fixpoint delete_max_aux (m: key) (p: priqueue) : priqueue * priqueue :=
   match p with
   | Leaf :: p' => let (j,k) := delete_max_aux m p' in (Leaf::j, k)
@@ -109,7 +148,11 @@ Definition delete_max (q: priqueue) : option (key * priqueue) :=
                             in Some (m, join p' q' Leaf)
   end.
 
+(* Para facilitar a utilização do join, é criada uma função auxiliar chamada merge. *)
+
 Definition merge (p q: priqueue) := join p q Leaf.
+
+(* Essa função diz se uma árvore é uma pow2heap, ela verifica se o nó raiz tem uma subárvore vazia a direita e se possui uma subárvore completa a esquerda. *)
 
 Fixpoint pow2heap' (n: nat) (m: key) (t: tree) :=
  match n, m, t with
@@ -126,6 +169,8 @@ Definition pow2heap (n: nat) (t: tree) :=
   | _ => False
   end.
 
+(* Para saber se realmente uma lista de árvores é uma priqueue, temos que garantir que cada árvore seja uma pow2heap de tamanho único, assim a cada nova árvore avaliada na lista, o tamanho é incrementado e verificado se é uma pow2heap daquele tamanho. *)
+
 Fixpoint priq' (i: nat) (l: list tree) : Prop :=
    match l with
   | t :: l'=> (t = Leaf \/ pow2heap i t) /\ priq' (S i) l'
@@ -134,8 +179,12 @@ Fixpoint priq' (i: nat) (l: list tree) : Prop :=
 
 Definition priq (q: priqueue) : Prop := priq' 0 q.
 
+(* A prova de uma priqueue vazia é trivial, já que foi definida que uma lista vazia é uma priqueue. *)
+
 Theorem empty_priq : priq empty.
 Proof. reflexivity. Qed.
+
+(* Para provar que uma junção de duas pow2heap de tamanho n é valida, é necessário checar se a junção das duas leva a uma pow2heap de tamanho n+1. Isso é feito tomando os casos base de cada árvore e checando se de fato a propriedade da max heap é satisfeita. *)
 
 Theorem smash_valid : forall (n: nat) (t: tree) (u: tree),
   pow2heap n t -> pow2heap n u -> pow2heap (S n) (smash t u).
@@ -156,23 +205,13 @@ Proof.
    - inversion H.
 Qed.
 
-Theorem priq'_valid: forall (n: nat) (t: tree) (l: priqueue),
-  priq' (S n) l -> (t = Leaf \/ pow2heap n t) -> priq' n (t :: l).
-Proof.
-  intros. destruct t.
-    - simpl. split.
-      + apply H0.
-      + apply H.
-    - simpl. split.
-      + left. reflexivity.
-      + apply H. 
-Qed.
+(* Um carry é valido para toda a priqueue q de tamanho n, para uma árvore t sendo pow2heap ou uma árvore vazia, o resultado do carry é uma priqueue de tamanho n. Isso é feito aplicando uma indução em q e sempre reduzindo t para casos conhecidos, tudo feito sem necessariamente saber, induzir ou destruir o tamanho (n) das priqueues. *)
 
 Theorem carry_valid : forall (n: nat) (q: priqueue),
   priq' n q -> forall (t: tree), (t = Leaf \/ pow2heap n t) ->
   priq' n (carry q t).
 Proof.
-  intros n q. generalize dependent n. induction q.
+  intros n q. revert n. induction q.
     - intros. destruct t.
       + unfold carry. simpl. split.
         * apply H0.
@@ -198,10 +237,12 @@ Proof.
         * inversion H. apply H2.
 Qed.
 
+(* Ao fazer uma inserção de um natural em uma priqueue q, ela deve continuar sendo uma priqueue. A prova deve ser feita utilizando o caso base de uma priqueue vazia, e ao ser feita a inserção numa árvore, pode ser utilizada a prova carry_valid. *)
+
 Theorem insert_priq : forall (x: nat) (q: priqueue),
   priq q -> priq (insert x q).
 Proof.
-  intros. induction q.
+  intros. destruct q.
   - unfold priq. simpl. split.
     + right. apply I.
     + apply I.
@@ -211,6 +252,8 @@ Proof.
       * apply H1.
     + simpl. intuition.
 Qed.
+
+(*  *)
 
 Theorem join_valid: forall (p: priqueue) (q: priqueue) (n: nat) (c: tree),
   priq' n p -> priq' n q -> (c = Leaf \/ pow2heap n c) -> priq' n (join p q c).
@@ -324,29 +367,11 @@ Proof.
             { inversion H0. apply H3. }
             { left. reflexivity. } }
 Qed.
-      
+
+(* Ao fazer um merge entre duas priqueue, o resultado deve ser uma priqueue. Isso é facilmente feito utilizando a prova para o join, já que merge é definido em a partir do join. *)
+
 Theorem merge_priq: forall (p: priqueue) (q: priqueue),
   priq p -> priq q -> priq (merge p q).
 Proof.
   intros. unfold merge. apply join_valid; auto.
 Qed.
-
-Require Import Coq.Sorting.Permutation.
-
-Inductive tree_elems: tree -> list key -> Prop :=
-| tree_elems_leaf: tree_elems Leaf nil
-| tree_elems_node: forall (bl: list key) (br: list key) (v: nat)
-                   (tl: tree) (tr: tree) (b: list key),
-           tree_elems tl bl ->
-           tree_elems tr br ->
-           Permutation b (v::bl++br) ->
-           tree_elems (Node v tl tr) b.
-
-Theorem tree_elems_ext: forall (t: tree) (e1: list key) (e2: list key),
-  Permutation e1 e2 -> tree_elems t e1 -> tree_elems t e2.
-Proof.
-  intros. destruct e2 as [| e e']. 
-    - destruct e1 as [| a a'].
-      + apply H0.
-      + destruct t.
-        * inversion H0.
